b0e08d4cbdd64c1d7855820d7e7e73973f70d8a9
mashies <victor.foggon@gmail.com>
2024-04-08 12:14:46 +1000

    Added 'count_letters_in_string' function and cleaned up 'count_words_in_string' function.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 def read_contents_from_file(filepath):
     with open(filepath) as file:
-        file_contents = file.read()
-    return file_contents
+        return file.read()
     
+def count_words_in_string(string):
+    split_string_as_list = string.split()
+    return len(split_string_as_list)
+
+def count_letters_in_string(string):
+    letter_count = {}
+    lowercase_string = string.lower()
+    for letter in lowercase_string:
+        if letter not in letter_count:
+            letter_count[letter] = 0
+        letter_count[letter] += 1
+    return letter_count
+
 def main():
     frankenstein = read_contents_from_file("books/frankenstein.txt")
-    print(frankenstein)
+    frankenstein_word_count = count_words_in_string(frankenstein)
+    frankenstein_letter_count = count_letters_in_string(frankenstein)
+    print(f"{frankenstein_word_count} words found in the document.")
+    print(frankenstein_letter_count)
 
 main()
